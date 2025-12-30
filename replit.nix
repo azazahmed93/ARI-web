e9822e46cc6f5cd836a0f594e48539f487f74c69
Atif <atif.sik252@gmail.com>
2025-12-30 17:04:50 +0500

    Config: Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 {pkgs}: {
   deps = [
+    # Existing dependencies
     pkgs.qhull
     pkgs.pkg-config
     pkgs.gtk3
@@ -19,5 +20,26 @@
     pkgs.lcms2
     pkgs.freetype
     pkgs.glibcLocales
+
+    # Chromium/Playwright dependencies
+    pkgs.chromium
+    pkgs.nss
+    pkgs.nspr
+    pkgs.atk
+    pkgs.at-spi2-atk
+    pkgs.cups
+    pkgs.libdrm
+    pkgs.libxkbcommon
+    pkgs.xorg.libXcomposite
+    pkgs.xorg.libXdamage
+    pkgs.xorg.libXfixes
+    pkgs.xorg.libXrandr
+    pkgs.mesa
+    pkgs.alsa-lib
+    pkgs.pango
+    pkgs.glib
+    pkgs.gdk-pixbuf
+    pkgs.expat
+    pkgs.dbus
   ];
 }
